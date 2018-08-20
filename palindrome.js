f4063376060778ba077df05111b25c69931bc8dd
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-08-20 08:25:47 -0700

    Commented steps

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,13 @@
-console.log("script connected");
+function palindrome(str) {
+    // Remove all non-alphanumeric characters
+	// Turn all letters to lower case
+	// Find length of string and divide in 2
+	// Flip second half
+	// If half 2 === half 1, it's a palindrome
+	return true;
+}
+
+
+
+var output = palindrome("eye");
+console.log(output);
