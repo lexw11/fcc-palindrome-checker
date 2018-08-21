ae7cabdae62d2530cef30f27a0f6c8d21f32c0ea
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-08-20 17:11:36 -0700

    Removed all non-alphanumeric characters

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,23 @@
 function palindrome(str) {
     // Remove all non-alphanumeric characters
+    var regex = /[^_\W]/;
+    return str.split('')
+    		.filter(char => regex.test(char))
+    		.join("");
 	// Turn all letters to lower case
-	// Find length of string and divide in 2
+	// Find length  of string and divide in 2
 	// Flip second half
 	// If half 2 === half 1, it's a palindrome
-	return true;
+	// return true;
 }
 
 
 
 var output = palindrome("eye");
+var output2 = palindrome("A man, a plan, a canal. Panama");
+var output3 = palindrome("My age is 0, 0 si ega ym.");
+var output4 = palindrome("0_0 (: /-\ :) 0-0");
 console.log(output);
+console.log(output2);
+console.log(output3);
+console.log(output4);
